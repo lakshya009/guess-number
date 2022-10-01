a609a8e0a82010aff56679dd75f362b10312f945
Sarthak Nigam <srthkngm@gmail.com>
2022-10-01 11:24:53 +0530

    Minor changes in the ui

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ var guessedNumber = [];
 function play(){
     var userGuess = document.getElementById('guess').value;
     if(userGuess < 1 || userGuess > 100){
-        alert("Please Enter Number in given Range");
+        alert("Please Input Valid Number");
     }
     else{
         guessedNumber.push(userGuess);
         noOfGuesses += 1;
         if(noOfGuesses == 10){
             nog.textContent = "Game Over ! You Lost... Refresh to Play Again :)";
-            gn.textContent = "No. of Guesses: " + noOfGuesses + ".     The Number was: " + answer;
-            msg.textContent = "Guessed Numbers are: " + guessedNumber;
+            gn.textContent = "No. of Guesses: " + noOfGuesses + ".  &   The Number was: " + answer;
+            msg.textContent = "You Picked: " + guessedNumber;
             document.getElementById("btn").disabled = true;
 
         }
         else if(userGuess < answer){
-            nog.textContent = "Your Guess is too LOW."
+            nog.textContent = "Your Guess is LOW."
             gn.textContent = "No. of Guesses: " + noOfGuesses;
             msg.textContent = "Guessed Numbers are: " + guessedNumber;
         }
         
         else if(userGuess > answer){
-            nog.textContent = "Your Guess is too HIGH."
+            nog.textContent = "Your Guess is HIGH."
             gn.textContent = "No. of Guesses: " + noOfGuesses;
             msg.textContent = "Guessed Numbers are: " + guessedNumber;
         }
         else if(userGuess == answer){
-            nog.textContent = "Yay! You Won. Refresh to Restart the Game !"
+            nog.textContent = "YOHO! You Won. Refresh to Restart the Game ! 😉"
             gn.textContent = "No. of Guesses: " + noOfGuesses + ".     The Number was: " + answer;
             msg.textContent = "Guessed Numbers are: " + guessedNumber;
             document.getElementById("btn").disabled = true;
